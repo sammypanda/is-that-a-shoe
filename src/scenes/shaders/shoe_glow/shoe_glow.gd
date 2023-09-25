b49cefd0ae5a344868d48a6b785999249e215f7b
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-09-25 21:37:46 +1000

    shoe_glow: changed to simple light

--- src/scenes/shaders/shoe_glow/shoe_glow.gd
+++ src/scenes/shaders/shoe_glow/shoe_glow.gd
@@ -2,10 +2,11 @@ extends Node3D
 
 @export var player : CharacterBody3D
 @export var shoe_distance_threshold : int
-@onready var child_mesh : MeshInstance3D = get_node("Billboard")
+@onready var camera_control
+var looked_at : bool = false
 
 func _ready():
-	child_mesh.visible = false
+	self.visible = false
 	assert(shoe_distance_threshold != 0 || null, "The shoe distance threshold is unset or 0")
 	assert(player != null, "The player object is not defined")
 
@@ -14,4 +15,4 @@ func _physics_process(_delta):
 		player.global_transform.origin
 	) > shoe_distance_threshold
 	
-	child_mesh.visible = sparkle_event # only show 'Shoe' mesh when 'sparkle_event' time
+	self.visible = sparkle_event # only show 'Shoe' mesh when 'sparkle_event' time
