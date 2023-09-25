f4eb855d3da95a921c961831ac6f9522093319fc
sammypanda <38725335+sammypanda@users.noreply.github.com>
2023-09-25 16:12:12 +1000

    show_glow: shader placeholder created

--- src/scenes/shaders/shoe_glow/shoe_glow.gd
+++ src/scenes/shaders/shoe_glow/shoe_glow.gd
@@ -1,8 +1,13 @@
 extends Node3D
 
-@onready var player : CharacterBody3D = $"../Player"
-@onready var child_mesh : MeshInstance3D = get_node("ShoeBoundary")
+@export var player : CharacterBody3D
 @export var shoe_distance_threshold : int
+@onready var child_mesh : MeshInstance3D = get_node("Billboard")
+
+func _ready():
+	child_mesh.visible = false
+	assert(shoe_distance_threshold != 0 || null, "The shoe distance threshold is unset or 0")
+	assert(player != null, "The player object is not defined")
 
 func _physics_process(_delta):
 	var sparkle_event : bool = global_transform.origin.distance_to(
